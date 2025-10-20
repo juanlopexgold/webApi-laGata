a781cf4cfd2b48dd06f98558fb72f78a59f88055
juanlopexgold <juanlopexdhegold@gmail.com>
2025-10-19 23:26:33 -0600

    Update assembly information and project paths in LaGata.Api, including version change and user-specific directory adjustments.

--- obj/Debug/net8.0/LaGata.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/LaGata.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LaGata.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebb76a3864832b885213cea0c6205dcdb3f4da1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5c072ce3d5ee6757319d68c678638b9c5a4b6b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("LaGata.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LaGata.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
